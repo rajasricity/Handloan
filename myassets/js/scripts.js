c7c64d8b0c7731221aeeb23daa299482a8f96943
Raja Sekhar Sana <sanarajasekhar@gmail.com>
2017-04-24 19:19:38 +0530

    Add files via upload

---
+++ myassets/js/scripts.js
@@ -0,0 +1,106 @@
+var server = "http://mukthasis.com/handloan/";
+$(document).ready(function(){
+  localStorage.setItem('collapsed_menu', "true");
+
+$("#login").on('submit', function(e){
+   e.preventDefault();
+   var fdata = $("#login").serialize();
+   $.ajax({
+      url:server+"checkLogin.php",
+      data:fdata,
+      type:"post",
+      dataType: "json",
+      beforeSend: function(){
+        $("#loader").show();
+      },
+      success: function(str){
+        $("#loader").hide();
+        if(str.Userno == ''){
+          $("#wmsg").show();
+          $("#mobile").val('');
+        }else{
+        $("#wmsg").hide();
+        localStorage.setItem("Userno",str.Userno);
+        localStorage.setItem("Role",str.Role);
+        if(str.Role == 'Lendee'){
+        location.href="Dashboard.html";
+        }else{
+         location.href="Dashboard.html";
+        }
+        }
+      },
+      error:function(qXHR, exception){
+      alert(jqXHR.status);
+      }
+   });
+});
+
+});
+
+function connect(){
+$.ajax({
+      url:server+"connect.php",
+      success: function(str){
+
+      },
+      error:function(jqXHR, exception){
+        var msg = '';
+        if (jqXHR.status === 0) {
+            msg = 'Not connect.\n Verify Network.';
+        } else if (jqXHR.status == 404) {
+            msg = 'Requested page not found. [404]';
+        } else if (jqXHR.status == 500) {
+            msg = 'Internal Server Error [500].';
+        } else if (exception === 'parsererror') {
+            msg = 'Requested JSON parse failed.';
+        } else if (exception === 'timeout') {
+            msg = 'Time out error.';
+        } else if (exception === 'abort') {
+            msg = 'Ajax request aborted.';
+        } else {
+            msg = 'Uncaught Error.\n' + jqXHR.responseText;
+        }
+        $('#conerr').html(msg);
+        $("#connection").modal("show");
+      }
+   });
+}
+
+function getUname(){
+  var fdata = {"Userno":localStorage.Userno };
+$.ajax({
+      url:server+"getUname.php",
+      type:"post",
+      data: fdata,
+      success: function(str){
+        $("#uname").html(str);
+      },
+      error:function(jqXHR, exception){
+        var msg = '';
+        if (jqXHR.status === 0) {
+            msg = 'Not connect.\n Verify Network.';
+        } else if (jqXHR.status == 404) {
+            msg = 'Requested page not found. [404]';
+        } else if (jqXHR.status == 500) {
+            msg = 'Internal Server Error [500].';
+        } else if (exception === 'parsererror') {
+            msg = 'Requested JSON parse failed.';
+        } else if (exception === 'timeout') {
+            msg = 'Time out error.';
+        } else if (exception === 'abort') {
+            msg = 'Ajax request aborted.';
+        } else {
+            msg = 'Uncaught Error.\n' + jqXHR.responseText;
+        }
+        $('#conerr').html(msg);
+        $("#connection").modal("show");
+      }
+   });
+}
+
+function signout(){
+  localStorage.removeItem("Userno");
+  localStorage.removeItem("Role");
+  localStorage.clear();
+  location.href="index.html";
+}
